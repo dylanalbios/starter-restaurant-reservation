af3f49eb66bdac880dce8254d523bed525af0128
dylanalbios <dylxanj430@gmail.com>
2023-09-12 16:56:47 -0700

    validateReservation function added to check required fields of reservation

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -19,6 +19,43 @@ async function list(req, res) {
   res.json({ date: response });
 };
 
+async function validateReservation(req, res, next) {
+  const data = req.body.data;
+  const requiredFields = [
+    "first_name",
+    "last_name",
+    "mobile_number",
+    "reservation_date",
+    "reservation_time",
+    "people",
+  ];
+
+  // Helper function to send error
+  const sendError = (message, status = 400) => next({ status, message });
+
+  // Check for required fields
+  const missingField = requiredFields.find(field => !data.hasOwnProperty(field) || data[field] === "");
+  if (missingField) return sendError(`Field required: '${missingField}'`);
+
+  // Date and Time validation
+  const dateTime = `${data.reservation_date} ${data.reservation_time}`;
+  if (Number.isNaN(Date.parse(dateTime))) {
+      return sendError("'reservation_date' or 'reservation_time' field are in incorrect format");
+  }
+
+  // People field validation
+  if (typeof data.people !== "number") return sendError("'people' field must be a number");
+  if (data.people < 1) return sendError("'people' field must be at least 1");
+
+  // Status field validation
+  if (data.status && data.status !== "booked") {
+      return sendError(`'status' field cannot be ${data.status}`);
+  }
+
+  next();
+}
+
+
 
 /**
  * Create handler for reservation resources
@@ -32,6 +69,6 @@ async function create(req, res) {
 };
 
 module.exports = {
-  list,
-  create,
+  list: [asyncErrorBoundary(list)],
+  create: [asyncErrorBoundary(validateReservation), asyncErrorBoundary(create)],
 };
