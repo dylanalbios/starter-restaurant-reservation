e65327f3cee521d9492534946f44c39bbcbbd64c
dylanalbios <dylxanj430@gmail.com>
2023-09-12 15:16:07 -0700

    create and list functions added

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,37 @@
+const service = require("./reservations.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+
+
 /**
  * List handler for reservation resources
  */
 async function list(req, res) {
-  res.json({
-    data: [],
-  });
-}
+  const date = req.query.date;
+  const mobile_number = req.query.mobile_number;
+
+  const reservations = await service.list(date, mobile_number);
+
+  const response = reservations.filter(
+    (reservation) => reservation.status !== "finished"
+  );
+
+  res.json({ date: response });
+};
+
+
+/**
+ * Create handler for reservation resources
+ */
+async function create(req, res) {
+  req.body.data.status = "booked";
+
+  const response = await service.create(req.body.data);
+
+  res.status(201).json({ data: response[0] });
+};
 
 module.exports = {
   list,
+  create,
 };
