c0bc04932874f7a93e469a43c9fc623057572dee
dylanalbios <dylxanj430@gmail.com>
2023-10-11 17:28:53 -0700

    added check for reservation and remade Route handlers

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -1,6 +1,7 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
+import useQuery from "../utils/useQuery" 
+import { listReservations } from "../utils/api";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
@@ -12,23 +13,47 @@ import { today } from "../utils/date-time";
  *
  * @returns {JSX.Element}
  */
+
+
 function Routes() {
+  const [reservations, setReservations] = useState([]);
+  const [reservationsError, setReservationsError] = useState(null);
+
+
+  const query = useQuery();
+  const date = query.get("date") || today();
+
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    listReservations({ date: date }, abortController.signal)
+    .then(setReservations)
+    .catch(setReservationsError);
+
+    return () => abortController.abort();
+  }, [date]);
+
   return (
     <Switch>
-      <Route exact={true} path="/">
+      <Route exact path={["/", "/reservations"]}>
         <Redirect to={"/dashboard"} />
       </Route>
-      <Route exact={true} path="/reservations">
-        <Redirect to={"/dashboard"} />
-      </Route>
+
       <Route path="/dashboard">
-        <Dashboard date={today()} />
+        <Dashboard 
+          date={date} 
+          reservations={reservations} 
+          reservationsError={reservationsError} 
+          loadDashboard={() => setReservations([])} 
+        />
       </Route>
+
       <Route>
         <NotFound />
       </Route>
     </Switch>
-  );
+  )
 }
 
 export default Routes;
