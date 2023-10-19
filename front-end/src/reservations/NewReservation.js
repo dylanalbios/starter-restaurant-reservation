988a99c0d098301d434af0eb243d2f3cd4c703dc
dylanalbios <dylxanj430@gmail.com>
2023-10-18 17:52:14 -0700

    NewReservation function remade

--- front-end/src/reservations/NewReservation.js
+++ front-end/src/reservations/NewReservation.js
@@ -10,11 +10,9 @@ export default function NewReservation({ loadDashboard, edit }) {
   const history = useHistory();
   const { reservation_id } = useParams();
 
-  const [errors, setErrors] = useState({
-    api: null, 
-    validation: [], 
-    reservation: null,
-  });
+  const [errors, setErrors] = useState([]);
+  const [reservationError, setReservationError] = useState(null);
+  const [apiError, setApiError] = useState(null);
 
   const [formData, setFormData] = useState({ 
     first_name: "",
@@ -25,104 +23,158 @@ export default function NewReservation({ loadDashboard, edit }) {
     people: 1,
   });
 
+
   useEffect(() => {
-    async function fetchandFillRsv() {
+    if (edit) {
+      if (!reservation_id) return null;
+
+      loadReservations()
+        .then((response) =>
+          response.find(
+            (reservation) =>
+              reservation.reservation_id === Number(reservation_id)
+          )
+        )
+        .then(fillFields);
+    }
+
+    function fillFields(foundReservation) {
+      if (!foundReservation || foundReservation.status !== "booked") {
+        return <p>Only booked reservations can be edited.</p>;
+      }
+
+
+      setFormData({
+        first_name: foundReservation.first_name,
+        last_name: foundReservation.last_name,
+        mobile_number: foundReservation.mobile_number,
+        reservation_date: foundReservation.reservation_date,
+        reservation_time: foundReservation.reservation_time,
+        people: foundReservation.people,
+      });
+    }
+
+    async function loadReservations() {
       const abortController = new AbortController();
 
-      try {
-        const reservations = await listReservations({}, abortController.signal);
-        const foundReservation = reservations.find(
-          (res) => res.reservation_id === Number(reservation_id)
-        );
-        if (foundReservation && foundReservation.status === "booked") {
-          setFormData({ ...foundReservation });
-        } else {
-          setErrors((prev) => ({
-            ...prev,
-            reservation: "Only booked reservations can be edited."
-          }));
-        }
-      } catch (err) {
-        setErrors((prev) => ({ ...prev, api: err }));
-      }
+      return await listReservations(abortController.signal).catch(
+        setReservationError
+      );
     }
-
-    if (edit && reservation_id) fetchandFillRsv();
   }, [edit, reservation_id]);
 
+
+
+
   function handleInputChange({ target }) {
-    const value = target.name === 'people' ? Number(target.value): target.value;
-    setFormData((prev) => ({ ...prev, [target.name]: value}));
+    setFormData({
+      ...formData,
+      [target.name]:
+        target.name === "people" ? Number(target.value) : target.value,
+    });
   };
 
 
-  async function handleSubmit(event) {
+
+
+  function handleSubmit(event) {
     event.preventDefault();
+    const abortController = new AbortController();
 
-    const validationErrors = validateFormData();
+    const foundErrors = validateFields().concat(validateDate());
 
-    if (validationErrors.length === 0) {
-      try {
-        const abortController = new AbortController();
-        if (edit && reservation_id) {
-          await editReservation(reservation_id, formData, abortController.signal);
-        } else {
-          await createReservation(formData, abortController.signal);
-        }
-        loadDashboard();
-        history.push(`/dashboard?date=${formData.reservation_date}`);
-      } catch (apiError) {
-        setErrors((prev) => ({ ...prev, api: apiError}))
+    if (foundErrors.length === 0) {
+      if (edit && reservation_id) {
+        editReservation(reservation_id, formData, abortController.signal)
+          .then(loadDashboard)
+          .then(() => 
+            history.push(`/dashboard?date=${formData.reservation_date}`)
+          )
+          .catch(setApiError);
+      } else {
+        createReservation( formData, abortController.signal)
+          .then(loadDashboard)
+          .then(() => 
+            history.push(`/dashboard?date=${formData.reservation_date}`)
+          )
+          .catch(setApiError);
       }
     } else {
-      setErrors((prev) => ({ ...prev, validation: validationErrors}))
+      setErrors(foundErrors);
     }
+
+    setErrors(foundErrors);
+
+    return () => abortController.abort();
   };
 
-  function validateFormData() {
-    const errorMessage = [];
 
-    if (formData.first_name.trim() === "") {
-      errorMessage.push("First Name must be filled")
+
+  function validateFields() {
+    const foundErrors = [];
+    for (const field in formData) {
+      if (formData[field] === "") {
+        foundErrors.push({
+          message: `${field.split("_").join(" ")} must be filled in.`,
+        });
+      }
     }
+    return foundErrors;
+  };
 
-    if (formData.last_name.trim() === "") {
-      errorMessage.push("Last Name must be filled")
-    }
 
-    const phonePattern = /^[2-9]{1}[0-9]{9}$/;
-    if(!phonePattern.test(formData.mobile_number)) {
-      errorMessage.push("Mobile number must be 10 digits and cannot start with 0 or 1.");
-    }
 
-    const reservationDate = new Date(formData.reservation_date).getTime();
+  function validateDate() {
+    const foundErrors = [];
+    const reservedDate = new Date(
+      `${formData.reservation_date}T${formData.reservation_time}:00.000`
+    );
     const todaysDate = new Date();
-    if (reservationDate < todaysDate) {
-      errorMessage.push("Reservation date must be today or in the future.")
+
+    if (reservedDate.getDay() === 2) {
+      foundErrors.push({
+        message: "Reservation on a tuesday"
+      });
     }
 
-    const reservationTime = formData.reservation_time;
-    if (reservationTime < "10:30" || reservationTime > "21:30") {
-      errorMessage.push("Reservation Time must be between 10:30AM and 9:30PM")
+    if (reservedDate < todaysDate) {
+      foundErrors.push({
+        message: "Reservation in the past"
+      });
     }
 
-    if (formData.people < 1) {
-      errorMessage.push("Party size must be at least 1 person.")
+    const hour = reservedDate.getHours();
+    const minute = reservedDate.getMinutes();
+
+    if (hour < 10 || (hour === 10 && minute < 30)) {
+      foundErrors.push({
+        message: "Reservation not open til 10:30"
+      });
+    } else if (hour === 22 && minute >= 30) {
+      foundErrors.push({
+        message: "Reservation closed after 10:30"
+      });
+    } else if (hour === 21 && minute > 30) {
+      foundErrors.push({
+        message: "Reservation made an hour before close"
+      });
     }
 
-    return errorMessage;
-  }
+    return foundErrors;
+  };
+
+
 
 
   return (
     <form onSubmit={handleSubmit}>
 
       {/* Display Errors */}
-      {errors.validation.map((error, index) => (
-        <ErrorAlert key={index} error={{ message: error }} />
+      {errors.map((error, index) => (
+        <ErrorAlert key={index} error={error} />
       ))}
-      <ErrorAlert error={errors.api} />
-      <ErrorAlert error={{ message: errors.reservation }} />
+      <ErrorAlert error={apiError} />
+      <ErrorAlert error={reservationError} />
 
       {/* Form */}
       <label className="form-label" htmlFor="first_name">First Name:</label>
