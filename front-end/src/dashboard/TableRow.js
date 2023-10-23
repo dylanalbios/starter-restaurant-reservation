39cfd4c611d4397547fc318017819d3115ea26d7
dylanalbios <dylxanj430@gmail.com>
2023-10-22 20:03:10 -0700

    ability to finish table added

--- front-end/src/dashboard/TableRow.js
+++ front-end/src/dashboard/TableRow.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { finishTable } from "../utils/api";
 
 export default function TableRow({ table, loadDashboard}) {
     const {
@@ -12,14 +13,35 @@ export default function TableRow({ table, loadDashboard}) {
     // Don't return the table if it's finished or doesn't exist.
     if (!table) return null;
 
+    async function handleTableFinish() {
+        if (window.confirm("Is this table ready to seat new guests? This action cannot be undone.")) {
+            try {
+                const abortController = new AbortController();
+                await finishTable(table.table_id, abortController.signal);
+                loadDashboard(); // ensure this function refreshes the data correctly
+            } catch (error) {
+                console.error("Error finishing table:", error);
+            }
+        }
+    }
+
 
     return (
         <tr>
             <th scope="row">{table_id}</th>
             <td>{table_name}</td>
             <td>{capacity}</td>
-            <td>{status}</td>
-            <td>{reservation_id}</td>
+            <td data-table-id-status={table.table_id}>{status}</td>
+            <td>{reservation_id ? reservation_id : "--"}</td>
+            {status === "occupied" && (
+                <td>
+                    <button
+                    data-table-id-finish={table_id}
+                    onClick={handleTableFinish}
+                    type="button"
+                >Finish</button>
+                </td>
+            )}
         </tr>
     );
 };
