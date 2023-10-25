30fdae4092f0a17e843e9ba08260ec5d7554a7a2
dylanalbios <dylxanj430@gmail.com>
2023-10-24 18:49:30 -0700

    cors added with vercel URL

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,6 +2,23 @@ const { PORT = 5001 } = process.env;
 
 const app = require("./app");
 const knex = require("./db/connection");
+const cors = require("cors");
+
+const allowedOrigins = [
+  "https://starter-restaurant-reservation-i7cmxsao2-dylanalbios.vercel.app"
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.included(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 
 knex.migrate
   .latest()
