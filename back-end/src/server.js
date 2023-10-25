80dca5c444e9a5c08f95ba369fae9bdc4864557b
dylanalbios <dylxanj430@gmail.com>
2023-10-24 19:01:08 -0700

    typo

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -10,7 +10,7 @@ const allowedOrigins = [
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.included(origin) || !origin) {
+    if (allowedOrigins.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
